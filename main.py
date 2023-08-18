112d9025b5900be7ba5d84c79b0e8426a3de857c
SX-7 <sn1.test.preria.2002@gmail.com>
2023-08-18 14:31:17 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,69 @@
+import pyxel as px
+from utils import Vec3, Camera, Rot3
+import math
+
+
+class App:
+    def __init__(self) -> None:
+        px.init(128, 128)
+        self.camera_position = Vec3(0, 32, 0)
+        self.camera_rotation = Rot3(math.pi / 2, 0, 0)
+        self.display_surface = Vec3(64, 64, 32)
+        self.camera = Camera(
+            self.camera_position, self.camera_rotation, self.display_surface
+        )
+        self.screen_position = []
+        self.point_data = []
+        for x in range(-16, 16, 2):
+            for y in range(-16, 16, 2):
+                self.point_data.append(
+                    Vec3(x, y, (px.noise(x, y, 0.5) + 1) * 8)
+                )
+
+        px.run(self.update, self.draw)
+
+    def update(self):
+        if px.btn(px.KEY_A):
+            self.camera_position += Vec3(-1, 0, 0)
+        if px.btn(px.KEY_D):
+            self.camera_position += Vec3(1, 0, 0)
+        if px.btn(px.KEY_W):
+            self.camera_position += Vec3(0, -1, 0)
+        if px.btn(px.KEY_S):
+            self.camera_position += Vec3(0, 1, 0)
+        if px.btn(px.KEY_R):
+            self.camera_position += Vec3(0, 0, -1)
+        if px.btn(px.KEY_F):
+            self.camera_position += Vec3(0, 0, 1)
+        if px.btn(px.KEY_LEFT):
+            self.camera_rotation += Rot3(0, 0, -1) / 10
+        if px.btn(px.KEY_RIGHT):
+            self.camera_rotation += Rot3(0, 0, 1) / 10
+        if px.btn(px.KEY_UP):
+            self.camera_rotation += Rot3(1, 0, 0) / 10
+        if px.btn(px.KEY_DOWN):
+            self.camera_rotation += Rot3(-1, 0, 0) / 10
+        if px.btn(px.KEY_Q):
+            self.camera_rotation += Rot3(0, 1, 0) / 10
+        if px.btn(px.KEY_E):
+            self.camera_rotation += Rot3(0, -1, 0) / 10
+        self.camera.update_camera(
+            self.camera_position, self.camera_rotation, self.display_surface
+        )
+        self.screen_position = []
+        for point in self.point_data:
+            self.screen_position.append(self.camera.get(point))
+
+    def draw(self):
+        px.cls(0)
+        for point in self.screen_position:
+            px.pset(point.x, point.y, 13)
+        px.text(0, 0, f"X={self.camera_position.x}", 2)
+        px.text(0, 6, f"Y={self.camera_position.y}", 2)
+        px.text(0, 12, f"Z={self.camera_position.z}", 2)
+        px.text(64, 0, f"Rx={self.camera_rotation.x}", 2)
+        px.text(64, 6, f"Ry={self.camera_rotation.y}", 2)
+        px.text(64, 12, f"Rz={self.camera_rotation.z}", 2)
+
+
+App()
---
+++ utils.py
@@ -0,0 +1,215 @@
+import math
+
+
+def _verify_type(compared_object: object, *types: type) -> int:
+    """Throws a descriptive error when types of two objects don't align.
+    Returns an int corrensponding to position of the type in `types` if
+    they do, starting from 0. Remember that passing *tuple into `types`
+    unpacks the tuple into the function.
+
+    Args:
+        compared_object (object): Object to check the type of
+        *types (type): Types of the object to be checked against
+    Raises:
+        TypeError: Incorrect type
+    Returns:
+        position (int): Which one of the requested types to check the object is
+    """
+    if not isinstance(compared_object, types):
+        raise TypeError(
+            f"{compared_object.__class__} is not one of the supported types: {types}"  # noqa: E501
+        )
+    else:
+        for i in range(len(types)):
+            if isinstance(compared_object, types[i]):
+                return i
+
+
+class Vec2:
+    def __init__(self, x: float | int, y: float | int) -> None:
+        self.x: float = float(x)
+        self.y: float = float(y)
+
+    def __add__(self, other):
+        return Vec2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vec2(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        _verify_type(other, int, float)
+        return Vec2(self.x * other, self.y * other)
+
+    def __truediv__(self, other):
+        return self.__mul__(1 / other)
+
+    def __neg__(self):
+        return self.__mul__(-1)
+
+
+class Vec3:
+    def __init__(self, x: float | int, y: float | int, z: float | int) -> None:
+        self.x: float = float(x)
+        self.y: float = float(y)
+        self.z: float = float(z)
+
+    def __add__(self, other):
+        return Vec3(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other):
+        return Vec3(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __mul__(self, other):
+        _verify_type(other, int, float)
+        return Vec3(self.x * other, self.y * other, self.z * other)
+
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y + self.z * other.z
+
+    def cross(self, other):
+        return Vec3(
+            self.y * other.z - self.z * other.y,
+            self.z * other.x - self.x * other.z,
+            self.x * other.y - self.y * other.x,
+        )
+
+    def __truediv__(self, other):
+        return self.__mul__(1 / other)
+
+    def __neg__(self):
+        return self.__mul__(-1)
+
+
+class Mat3:
+    """List of lists.
+    Structure is: [[row1_data][r2_d][r3_d]].
+    First row is the upmost one, first cell in it is leftmost one.
+    """
+
+    def __init__(self, matrix_data: [[float], [float], [float]]) -> None:
+        self.matrix = matrix_data
+
+    def __mul__(self, other):
+        choice = _verify_type(other, Mat3, int, float, Vec3)
+        match choice:
+            case 0:
+                for x in range(3):
+                    for y in range(3):
+                        self.matrix[x][y] = (
+                            self.matrix[x][y] * other.matrix[y][x]
+                        )  # noqa
+            case 1 | 2:
+                for row in self.matrix:
+                    for cell in row:
+                        cell = cell * other
+            case 3:
+                return Vec3(
+                    self.matrix[0][0] * other.x
+                    + self.matrix[0][1] * other.y
+                    + self.matrix[0][2] * other.z,
+                    self.matrix[1][0] * other.x
+                    + self.matrix[1][1] * other.y
+                    + self.matrix[1][2] * other.z,
+                    self.matrix[2][0] * other.x
+                    + self.matrix[2][1] * other.y
+                    + self.matrix[2][2] * other.z,
+                )
+            case _:
+                raise IndexError
+        return self
+
+
+class Rot3:
+    """Rotation represented by set of Tait-Bryan angles, measured in radians.
+    Similar to Vec3, but with different sete of functions
+    """
+
+    def __init__(self, x: float | int, y: float | int, z: float | int) -> None:
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def x_rotation(self) -> Mat3:
+        return Mat3(
+            [
+                [1, 0, 0],
+                [0, math.cos(self.x), math.sin(self.x)],
+                [0, -math.sin(self.x), math.cos(self.x)],
+            ]
+        )
+
+    def y_rotation(self) -> Mat3:
+        return Mat3(
+            [
+                [math.cos(self.y), 0, -math.sin(self.y)],
+                [0, 1, 0],
+                [math.sin(self.y), 0, math.cos(self.y)],
+            ]
+        )
+
+    def z_rotation(self) -> Mat3:
+        return Mat3(
+            [
+                [math.cos(self.z), math.sin(self.z), 0],
+                [-math.sin(self.z), math.cos(self.z), 0],
+                [0, 0, 1],
+            ]
+        )
+
+    def __add__(self, other):
+        return Rot3(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __mul__(self, other):
+        _verify_type(other, int, float)
+        return Rot3(self.x * other, self.y * other, self.z*other)
+
+    def __truediv__(self, other):
+        return self.__mul__(1 / other)
+
+
+class Camera:
+    """Changes 3d coordinates to 2d ones"""
+
+    def __init__(
+        self,
+        camera_position: Vec3,
+        camera_orientation: Rot3,
+        display_surface: Vec3,
+    ) -> None:
+        self.update_camera(
+            camera_position, camera_orientation, display_surface
+        )
+
+    def update_camera(
+        self,
+        camera_position: Vec3,
+        camera_orientation: Rot3,
+        display_surface: Vec3,
+    ) -> None:
+        self.camera_position = camera_position
+        self.camera_orientation = camera_orientation
+        self.display_surface = display_surface
+
+    def get(self, point_position) -> Vec2:
+        adjusted_point_position = self.camera_orientation.x_rotation() * (
+            self.camera_orientation.y_rotation()
+            * (
+                self.camera_orientation.z_rotation()
+                * (point_position - self.camera_position)
+            )
+        )
+        if adjusted_point_position.z <= 0:
+            return Vec2(-1, -1)
+        try:
+            return Vec2(
+                self.display_surface.z
+                / adjusted_point_position.z
+                * adjusted_point_position.x
+                + self.display_surface.x,
+                self.display_surface.z
+                / adjusted_point_position.z
+                * adjusted_point_position.y
+                + self.display_surface.y,
+            )
+        except ZeroDivisionError:
+            return Vec2(-1, -1)
