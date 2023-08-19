fba12c9c88714d909926dcf48f6cf6a51bbe4203
SX-7 <sn1.test.preria.2002@gmail.com>
2023-08-19 16:26:37 +0200

    Increased viability of changing resolution

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ class App:
         px.init(128, 128)
         self.camera_position = Vec3(0, 32, 0)
         self.camera_rotation = Vec3(math.pi / 2, 0, 0)
-        self.display_surface = Vec3(64, 64, 32)
+        self.display_surface = Vec3(
+            px.width // 2, px.height // 2, math.sqrt(px.height * px.width) // 4
+        )
         self.camera = Camera(
             self.camera_position, self.camera_rotation, self.display_surface
         )
         self.screen_position = []
         self.point_data = []
-        for x in range(-16, 16, 2):
-            for y in range(-16, 16, 2):
+        for x in range(-32, 32):
+            for y in range(-32, 32):
                 self.point_data.append(
-                    Vec3(x, y, (px.noise(x, y, 0.5) + 1) * 8)
+                    Vec3(x, y, (px.noise(x, y, 0.5) + 1) * 64)
                 )
 
         px.run(self.update, self.draw)
@@ -60,12 +62,12 @@ class App:
         px.cls(0)
         for point in self.screen_position:
             px.pset(point.x, point.y, 13)
-        px.text(0, 0, f"X={self.camera_position.x}", 2)
-        px.text(0, 6, f"Y={self.camera_position.y}", 2)
-        px.text(0, 12, f"Z={self.camera_position.z}", 2)
-        px.text(64, 0, f"Rx={self.camera_rotation.x}", 2)
-        px.text(64, 6, f"Ry={self.camera_rotation.y}", 2)
-        px.text(64, 12, f"Rz={self.camera_rotation.z}", 2)
+        px.text(0, 0, f"X={round(self.camera_position.x,2)}", 2)
+        px.text(0, 6, f"Y={round(self.camera_position.y,2)}", 2)
+        px.text(0, 12, f"Z={round(self.camera_position.z,2)}", 2)
+        px.text(0, 18, f"Rx={round(self.camera_rotation.x,2)}", 2)
+        px.text(0, 24, f"Ry={round(self.camera_rotation.y,2)}", 2)
+        px.text(0, 30, f"Rz={round(self.camera_rotation.z,2)}", 2)
 
 
 App()
