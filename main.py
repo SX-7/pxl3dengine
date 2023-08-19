f340b740891a9fa8b460742d3d835f43efeeb5ff
SX-7 <sn1.test.preria.2002@gmail.com>
2023-08-19 23:43:24 +0200

    Swapped to OpenGL-like perspective

--- main.py
+++ main.py
@@ -1,63 +1,54 @@
 import pyxel as px
-from utils import Vec3, Camera, speed_test
-import math
+from utils import Vec3, Camera, Mat4, Vec4  # noqa
 
 
 class App:
     def __init__(self) -> None:
-        px.init(128, 128)
-        self.camera_position = Vec3(0, 32, 0)
-        self.camera_rotation = Vec3(math.pi / 2, 0, 0)
-        self.display_surface = Vec3(
-            px.width // 2, px.height // 2, math.sqrt(px.height * px.width) // 4
-        )
-        self.camera = Camera(
-            self.camera_position, self.camera_rotation, self.display_surface
-        )
+        px.init(136, 145)
+        self.camera_position = Vec3(0, 0, -30)
+        self.camera = Camera()
         self.screen_position = []
         self.point_data = []
-        for x in range(-32, 32):
-            for y in range(-32, 32):
-                self.point_data.append(
-                    Vec3(x, y, (px.noise(x, y, 0.5) + 1) * 64)
-                )
-
+        for i in range(-10, 10):
+            self.point_data.append(Vec4(i, 10, 10, 1))
+            self.point_data.append(Vec4(i, -10, 10, 1))
+            self.point_data.append(Vec4(i, -10, -10, 1))
+            self.point_data.append(Vec4(i, 10, -10, 1))
+            self.point_data.append(Vec4(10, i, 10, 1))
+            self.point_data.append(Vec4(-10, i, 10, 1))
+            self.point_data.append(Vec4(-10, i, -10, 1))
+            self.point_data.append(Vec4(10, i, -10, 1))
+            self.point_data.append(Vec4(10, 10, i, 1))
+            self.point_data.append(Vec4(-10, 10, i, 1))
+            self.point_data.append(Vec4(-10, -10, i, 1))
+            self.point_data.append(Vec4(10, -10, i, 1))
+        self.object_rotation = Vec4(0, 1, 0, 0)
         px.run(self.update, self.draw)
 
-    @speed_test
+    # @speed_test
     def update(self):
         if px.btn(px.KEY_A):
-            self.camera_position += Vec3(-1, 0, 0)
-        if px.btn(px.KEY_D):
             self.camera_position += Vec3(1, 0, 0)
+        if px.btn(px.KEY_D):
+            self.camera_position += Vec3(-1, 0, 0)
         if px.btn(px.KEY_W):
-            self.camera_position += Vec3(0, -1, 0)
-        if px.btn(px.KEY_S):
             self.camera_position += Vec3(0, 1, 0)
+        if px.btn(px.KEY_S):
+            self.camera_position += Vec3(0, -1, 0)
         if px.btn(px.KEY_R):
-            self.camera_position += Vec3(0, 0, -1)
-        if px.btn(px.KEY_F):
             self.camera_position += Vec3(0, 0, 1)
-        if px.btn(px.KEY_LEFT):
-            self.camera_rotation += Vec3(0, 0, -1) / 10
-        if px.btn(px.KEY_RIGHT):
-            self.camera_rotation += Vec3(0, 0, 1) / 10
-        if px.btn(px.KEY_UP):
-            self.camera_rotation += Vec3(1, 0, 0) / 10
-        if px.btn(px.KEY_DOWN):
-            self.camera_rotation += Vec3(-1, 0, 0) / 10
-        if px.btn(px.KEY_Q):
-            self.camera_rotation += Vec3(0, 1, 0) / 10
-        if px.btn(px.KEY_E):
-            self.camera_rotation += Vec3(0, -1, 0) / 10
-        self.camera.update_camera(
-            self.camera_position, self.camera_rotation, self.display_surface
+        if px.btn(px.KEY_F):
+            self.camera_position += Vec3(0, 0, -1)
+        self.object_rotation.w += 0.1
+        self.screen_position = self.camera.get(
+            self.point_data,
+            camera_pos=self.camera_position,
+            screen_heigth=px.height,
+            screen_width=px.width,
+            rotation=self.object_rotation,
         )
-        self.screen_position = []
-        for point in self.point_data:
-            self.screen_position.append(self.camera.get(point))
 
-    @speed_test
+    # @speed_test
     def draw(self):
         px.cls(0)
         for point in self.screen_position:
@@ -65,9 +56,6 @@ class App:
         px.text(0, 0, f"X={round(self.camera_position.x,2)}", 2)
         px.text(0, 6, f"Y={round(self.camera_position.y,2)}", 2)
         px.text(0, 12, f"Z={round(self.camera_position.z,2)}", 2)
-        px.text(0, 18, f"Rx={round(self.camera_rotation.x,2)}", 2)
-        px.text(0, 24, f"Ry={round(self.camera_rotation.y,2)}", 2)
-        px.text(0, 30, f"Rz={round(self.camera_rotation.z,2)}", 2)
 
 
 App()
--- utils.py
+++ utils.py
@@ -49,6 +49,10 @@ class Vec2:
         return self.__mul__(-1)
 
 
+class Mat3:
+    pass
+
+
 class Vec3:
     def __init__(self, x: float | int, y: float | int, z: float | int) -> None:
         self.x: float = float(x)
@@ -88,6 +92,33 @@ class Vec3:
         self.z /= scaler
         return self
 
+    def x_rotation(self) -> Mat3:
+        return Mat3(
+            [
+                [1, 0, 0],
+                [0, math.cos(self.x), math.sin(self.x)],
+                [0, -math.sin(self.x), math.cos(self.x)],
+            ]
+        )
+
+    def y_rotation(self) -> Mat3:
+        return Mat3(
+            [
+                [math.cos(self.y), 0, -math.sin(self.y)],
+                [0, 1, 0],
+                [math.sin(self.y), 0, math.cos(self.y)],
+            ]
+        )
+
+    def z_rotation(self) -> Mat3:
+        return Mat3(
+            [
+                [math.cos(self.z), math.sin(self.z), 0],
+                [-math.sin(self.z), math.cos(self.z), 0],
+                [0, 0, 1],
+            ]
+        )
+
 
 class Mat3:
     """List of lists.
@@ -102,11 +133,14 @@ class Mat3:
         choice = _verify_type(other, Mat3, int, float, Vec3)
         match choice:
             case 0:
+                result = result = Mat3([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
                 for x in range(3):
                     for y in range(3):
-                        self.matrix[x][y] = (
-                            self.matrix[x][y] * other.matrix[y][x]
-                        )  # noqa
+                        for order in range(3):
+                            result.matrix[y][x] += (
+                                self.matrix[y][order] * other.matrix[order][x]
+                            )
+                self = result
             case 1 | 2:
                 for row in self.matrix:
                     for cell in row:
@@ -169,7 +203,7 @@ class Vec4:
     def __neg__(self):
         return self.__mul__(-1)
 
-    def get_rotation_mat(self) -> Mat4:
+    def rotation_matrix(self) -> Mat4:
         sin = math.sin(self.w)
         cos = math.cos(self.w)
         return Mat4(
@@ -206,7 +240,13 @@ class Vec4:
 
 class Mat4:
     def __init__(
-        self, matrix_data: [[float], [float], [float], [float]]
+        self,
+        matrix_data: [[float], [float], [float], [float]] = [
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+        ],
     ) -> None:
         self.matrix = matrix_data
 
@@ -214,11 +254,16 @@ class Mat4:
         choice = _verify_type(other, Mat4, int, float, Vec4)
         match choice:
             case 0:
+                result = Mat4(
+                    [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+                )
                 for x in range(4):
                     for y in range(4):
-                        self.matrix[x][y] = (
-                            self.matrix[x][y] * other.matrix[y][x]
-                        )
+                        for order in range(4):
+                            result.matrix[y][x] += (
+                                self.matrix[y][order] * other.matrix[order][x]
+                            )
+                self = result
             case 1 | 2:
                 for row in self.matrix:
                     for cell in row:
@@ -246,10 +291,10 @@ class Mat4:
                 raise IndexError
         return self
 
-    def identity_matrix(self):
+    def identity_matrix():
         return Mat4([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
 
-    def scaling_matrix(self, scaling_vector: Vec3):
+    def scaling_matrix(scaling_vector: Vec3):
         return Mat4(
             [
                 [scaling_vector.x, 0, 0, 0],
@@ -259,7 +304,7 @@ class Mat4:
             ]
         )
 
-    def translation_matrix(self, translation_vector: Vec3):
+    def translation_matrix(translation_vector: Vec3):
         return Mat4(
             [
                 [1, 0, 0, translation_vector.x],
@@ -269,8 +314,29 @@ class Mat4:
             ]
         )
 
+    # https://www.scratchapixel.com/lessons/3d-basic-rendering/perspective-and-orthographic-projection-matrix/opengl-perspective-projection-matrix.html # noqa
+    def perspective_matrix(fov: float, aspect: float, near: float, far: float):
+        nea = near
+        top = math.tan(fov / 2) * nea
+        bot = -top
+        rig = top * aspect
+        lef = bot * aspect
+        return Mat4(
+            [
+                [(2 * near) / (rig - lef), 0, (rig + lef) / (rig - lef), 0],
+                [0, (2 * nea) / (top - bot), (top + bot) / (top - bot), 0],
+                [
+                    0,
+                    0,
+                    -((far + nea) / (far - nea)),
+                    -((2 * far * nea) / (far - nea)),
+                ],
+                [0, 0, -1, 0],
+            ]
+        )
+
     def look_at(
-        self, camera_position: Vec3, camera_orientation: Vec3, up_vector: Vec3
+        camera_position: Vec3, camera_orientation: Vec3, up_vector: Vec3
     ) -> Mat4:
         right = up_vector.cross(camera_orientation).normalize()
         camera_up = camera_orientation.cross(right).normalize()
@@ -294,49 +360,67 @@ class Mat4:
 class Camera:
     """Changes 3d coordinates to 2d ones"""
 
-    def __init__(
+    def __init__(self):
+        pass
+
+    def get(
         self,
-        camera_position: Vec3,
-        camera_orientation: Vec3,
-        display_surface: Vec3,
-    ) -> None:
-        self.update_camera(
-            camera_position, camera_orientation, display_surface
+        entity: list[Vec4],
+        screen_width: int,
+        screen_heigth: int,
+        world_coordinates: Vec3 = Vec3(0, 0, 0),
+        rotation: Vec4 = Vec4(0, 0, 0, 0),
+        scaling: Vec3 = Vec3(1, 1, 1),
+        camera_pos: Vec3 = Vec3(64, 64, 0),
+    ):
+        world_matrix = Mat4.scaling_matrix(scaling)
+        world_matrix = rotation.rotation_matrix() * world_matrix
+        world_matrix = (
+            Mat4.translation_matrix(world_coordinates) * world_matrix
         )
 
-    def update_camera(
-        self,
-        camera_position: Vec3,
-        camera_orientation: Vec3,
-        display_surface: Vec3,
-    ) -> None:
-        self.camera_position = camera_position
-        self.camera_orientation = camera_orientation
-        self.display_surface = display_surface
+        view_matrix = Mat4.translation_matrix(camera_pos)
 
-    def get(self, point_position) -> Vec2:
-        adjusted_point_position = self.camera_orientation.x_rotation() * (
-            self.camera_orientation.y_rotation()
-            * (
-                self.camera_orientation.z_rotation()
-                * (point_position - self.camera_position)
-            )
+        perspective_matrix = Mat4.perspective_matrix(
+            math.radians(100), screen_width / screen_heigth, 0.1, 100
         )
-        if adjusted_point_position.z <= 0:
-            return Vec2(-1, -1)
-        try:
-            return Vec2(
-                self.display_surface.z
-                / adjusted_point_position.z
-                * adjusted_point_position.x
-                + self.display_surface.x,
-                self.display_surface.z
-                / adjusted_point_position.z
-                * adjusted_point_position.y
-                + self.display_surface.y,
+
+        result = []
+        for point in entity:
+            # (when I'm referring to objects, rn I mean pixels)
+            # rn we're local space, objects have small values and all, basic
+            # and unrotated, it's entirely possible to pre-do this step, but
+            # that depends on the object. rn not skipping for completion sake
+            point = world_matrix * point
+            point = Vec4(
+                point.x / point.w, point.y / point.w, point.z / point.w, 1
             )
-        except ZeroDivisionError:
-            return Vec2(-1, -1)
+            # world space, so the objects got into their position in the game
+            point = view_matrix * point
+            point = Vec4(
+                point.x / point.w, point.y / point.w, point.z / point.w, 1
+            )
+            # camera/view space, we've moved the objects in front of our camera
+            point = perspective_matrix * point
+            if (
+                (-point.w < point.x < point.w)
+                and (-point.w < point.y < point.w)
+                and (-point.w < point.z < point.w)
+            ):
+                point = Vec4(
+                    point.x / point.w, point.y / point.w, point.z / point.w, 1
+                )
+            else:
+                continue
+            # clip space, objects have been translated to -1 to 1 coordinates
+            # and clipped and perspective
+            point += Vec4(1, 1, 0, 0)
+            point.x *= screen_width/2
+            point.y *= screen_heigth/2
+            # viewport transform, so basically we move to the 128x128 space, or
+            # whatever the wievport is
+            result.append(point)
+        return result
 
 
 def speed_test(func):
