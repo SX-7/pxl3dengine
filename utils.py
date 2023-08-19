016196d42aed7f031ae6c7bf7c99d09486a136af
SX-7 <sn1.test.preria.2002@gmail.com>
2023-08-19 16:12:49 +0200

    Add timer

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import math
+import functools
+import time
 
 
 class Mat3:
@@ -196,3 +198,18 @@ class Camera:
             )
         except ZeroDivisionError:
             return Vec2(-1, -1)
+
+
+def speed_test(func):
+    """Print the runtime of the decorated function"""
+
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()  # 1
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()  # 2
+        run_time = end_time - start_time  # 3
+        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+        return value
+
+    return wrapper_timer
